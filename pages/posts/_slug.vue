0dd17502c2bcbf1a12af85ebd7b8002e9312000c
Nekika <hugo.jahnke@gmail.com>
2020-05-24 18:57:44 +0200

    Creating post layout, post list page, post page

---
+++ pages/posts/_slug.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="post">
+    <h1>{{ post.title }}</h1>
+    <nuxt-content :document="post"></nuxt-content>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'post',
+  async asyncData ({ params, $content }) {
+    const posts = await $content('posts').where({ slug: params.slug }).fetch()
+    return {
+      post: posts[0]
+    }
+  }
+}
+</script>
---
+++ pages/posts/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="posts">
+    <nuxt-link
+      v-for="post in posts"
+      :key="post.id"
+      :to="`/posts/${post.slug}`">
+      {{ post.title }}
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content }) {
+    const posts = await $content('posts').fetch()
+    return {
+      posts
+    }
+  }
+}
+</script>
