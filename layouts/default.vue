402db9d11993582441016dc97c3e9e9b4b816696
Nekika <hugo.jahnke@gmail.com>
2020-05-24 19:27:29 +0200

    Adding Header and Footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <Navbar></Navbar>
+    <Header></Header>
     <nuxt />
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import Navbar from '~/components/Navbar'
+import Header from '~/components/Header'
+import Footer from '~/components/Footer'
 export default {
   components: {
-    Navbar
+    Header,
+    Footer
   }
 }
 </script>
