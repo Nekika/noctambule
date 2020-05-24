20a1f63cf7293ed11858260a8ae56f2976976b35
Nekika <hugo.jahnke@gmail.com>
2020-05-24 23:53:08 +0200

    Creating the dynamic tag route It will be used to display every posts associated to a tag

---
+++ pages/tags/_name.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ params, $content }) {
+    const posts = await $content('posts').where({ tags: { $contains: params.name } }).fetch()
+    return {
+      posts
+    }
+  },
+  head () {
+    return {
+      title: `${this.$route.params.name} posts`
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
