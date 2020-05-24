874e1f5216145d83005605153238c6d3060f80ea
Nekika <hugo.jahnke@gmail.com>
2020-05-24 19:27:04 +0200

    Creating Header and including Navbar component

---
+++ components/Header/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Header>
+    <h2>Noctambule</h2>
+    <Navbar></Navbar>
+  </Header>
+</template>
+
+<script>
+import Navbar from './Navbar'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
