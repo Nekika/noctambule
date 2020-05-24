d71934dc472f1cfaffd7218877b00f11218daacd
Nekika <hugo.jahnke@gmail.com>
2020-05-25 00:19:57 +0200

    Creating Post Preview component

---
+++ components/Post/Preview.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="post-preview">
+    <h3>{{ post.title }}</h3>
+    <p>{{ post.summary }}</p>
+    <nuxt-link
+      v-for="(tag, index) in post.tags"
+      :key="index"
+      :to="`/tags/${tag}`">
+      #{{ tag }}
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['post']
+}
+</script>
+
+<style scoped>
+
+</style>
