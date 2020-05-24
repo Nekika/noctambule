8570d897bb5e226c86fc44302b63ae7445a64c59
Nekika <hugo.jahnke@gmail.com>
2020-05-24 18:53:29 +0200

    Creating Navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav>
+    <nuxt-link to="/">Home</nuxt-link>
+    <nuxt-link to="/posts">Posts</nuxt-link>
+    <nuxt-link to="/tags">Tags</nuxt-link>
+  </nav>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Navbar></Navbar>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import Navbar from '~/components/Navbar'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+</style>
