afa6123de443bc6d8bf09f56c15e9ffea5ece59c
Nekika <hugo.jahnke@gmail.com>
2020-05-24 18:54:51 +0200

    Creating tags

---
+++ pages/tags/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="tags">
+    <h1>Tags</h1>
+    <div v-for="(tag, index) in tags" :key="index">
+      <h3>#{{ tag.name }}</h3>
+      <p>{{ tag.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content }) {
+    const tags = await $content('tags').fetch()
+    return { tags }
+  }
+}
+</script>
