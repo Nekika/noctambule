875238bb4eb868e7a9675f444d14f7ffed4e27ff
Nekika <hugo.jahnke@gmail.com>
2020-05-25 23:50:03 +0200

    Creating Tags component
    Simply used to display a listof post tags as links

---
+++ components/Post/Tags.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="post-tags">
+    <nuxt-link
+      v-for="tag in tags"
+      :key="tag"
+      :to="`/tags/${tag}`"
+    >
+      #{{ tag }}
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tags: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
