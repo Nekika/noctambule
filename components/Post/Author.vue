9b1e002fb400da6c0160feb43448def13eff43fe
Nekika <hugo.jahnke@gmail.com>
2020-05-27 22:46:51 +0200

    Adding social networks links

--- components/Post/Author.vue
+++ components/Post/Author.vue
@@ -3,5 +3,10 @@
     <img src="~/assets/images/nekika.png" alt="Nekika">
     <p>Hugo « Nekika » Jahnke</p>
     <p><a href="mailto:nekika@noctambule.something">nekika@noctambule.something</a></p>
+    <p>
+      <a href="https://github.com/Nekika">GitHub</a>
+      <a href="https://www.linkedin.com/in/hugo-jahnke-a96ba8146/">LinkedIn</a>
+      <a href="https://twitter.com/HugoJahnke">Twitter</a>
+    </p>
   </div>
 </template>
