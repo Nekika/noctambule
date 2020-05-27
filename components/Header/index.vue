693887e212eaee1d7b8187e9cfc5e2d444e51d77
Nekika <hugo.jahnke@gmail.com>
2020-05-27 22:55:21 +0200

    Sticky header

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="grid grid-cols-2 items-center">
+  <header class="sticky top-0 inset-x-0 grid grid-cols-2 items-center">
     <nuxt-link to="/" tag="h2" class="font-logo text-4xl">
       Noctambule
     </nuxt-link>
