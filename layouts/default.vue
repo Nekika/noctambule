02bff6a75c635696883f49bfb97e6ce1e22a31bd
Nekika <hugo.jahnke@gmail.com>
2020-05-28 01:27:25 +0200

    Styling
    - Tailwind conf : adding colors & more spacing values
    - Layout : min height to fill the screen, new spacing values, bg and
    font colors

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-sm md:px-md lg:px-lg xl:px-xl">
+  <div class="min-h-screen px-4 md:px-16 lg:px-24 xl:px-32 bg-grey text-white">
     <Header />
     <nuxt />
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,30 @@
 */
 module.exports = {
   theme: {
+    colors: {
+      blue: '#7289DA',
+      grey: '#2C2F33',
+      white: '#FFFFFF'
+    },
     spacing: {
-      sm: '16px',
-      md: '64px',
-      lg: '96px',
-      xl: '128px'
+      0: '0',
+      1: '4px',
+      2: '8px',
+      3: '12px',
+      4: '16px',
+      5: '20px',
+      6: '24px',
+      8: '28px',
+      10: '40px ',
+      12: '48px ',
+      16: '64px ',
+      20: '80px ',
+      24: '96px ',
+      32: '128px',
+      40: '160px',
+      48: '192px',
+      56: '224px',
+      64: '256px'
     },
     fontFamily: {
       logo: ['Shadows Into Light', 'cursive'],
