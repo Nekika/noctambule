9d9ac9e46a54a60233e1eb877c69e70d34931989
Nekika <hugo.jahnke@gmail.com>
2020-05-28 22:25:24 +0200

    Styling Post preview

--- components/Post/Preview.vue
+++ components/Post/Preview.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="post-preview">
-    <h3>
-      <NLink :to="`/posts/${post.slug}`">
-        {{ post.title }}
-      </NLink>
-    </h3>
-    <p>{{ post.summary }}</p>
-    <Tags :tags="post.tags" />
-  </div>
+  <NLink :to="`/posts/${post.slug}`" tag="div" class="post-preview" :style="{ backgroundImage: coverSrc }">
+    <div class="information">
+      <h3>{{ post.title }}</h3>
+      <p class="summary">{{ post.summary }}</p>
+      <Tags :tags="post.tags" />
+    </div>
+  </NLink>
 </template>
 
 <script>
@@ -22,10 +20,43 @@ export default {
       type: Object,
       default: null
     }
+  },
+  computed: {
+    coverSrc () {
+      const cover = require(`~/assets/covers/${this.post.cover}`)
+      return `url(${cover})`
+    }
   }
 }
 </script>
 
 <style scoped>
+  .post-preview {
+    /* Position */
+    @apply relative;
 
+    /* Size */
+    @apply h-56;
+
+    /* Display */
+    @apply text-center;
+
+    /* Background */
+    @apply bg-cover;
+  }
+
+  .post-preview > .information {
+    /* Position */
+    @apply absolute bottom-0;
+
+    /* Display */
+    @apply p-2;
+
+    /* Background */
+    @apply bg-darkgrey;
+  }
+
+  .post-preview > .information > .summary {
+    @apply text-lightgrey;
+  }
 </style>
