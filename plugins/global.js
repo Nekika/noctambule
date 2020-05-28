93db64e03531cca3fea6f633d162c07c766e2c02
Nekika <hugo.jahnke@gmail.com>
2020-05-28 23:37:54 +0200

    Registering more global components

--- plugins/global.js
+++ plugins/global.js
@@ -3,10 +3,13 @@
  */
 
 import Vue from 'vue'
+
 import Header from '~/components/Header'
-import Preview from '~/components/Post/Preview'
 import Post from '~/components/Post'
+import Preview from '~/components/Post/Preview'
+import Tags from '~/components/Post/Tags'
 
 Vue.component('Header', Header)
 Vue.component('Preview', Preview)
 Vue.component('Post', Post)
+Vue.component('Tags', Tags)
