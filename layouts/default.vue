ab97534a7caabdecf904fbc93e9c22af70066f6e
Nekika <hugo.jahnke@gmail.com>
2020-05-30 00:59:42 +0200

    Removing padding bot in order to avoid useless scrolling

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-4 pb-10 md:px-24 lg:px-48 xl:px-64 bg-grey text-white">
+  <div class="px-4 md:px-24 lg:px-48 xl:px-64 bg-grey text-white">
     <Header />
     <nuxt class="mt-2" />
   </div>
