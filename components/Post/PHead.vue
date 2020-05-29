d813c97989c13f58ed5798c1c314d28789af54a1
Nekika <hugo.jahnke@gmail.com>
2020-05-30 00:11:20 +0200

    Removing Post cover This component was not really pertinent

--- components/Post/PHead.vue
+++ components/Post/PHead.vue
@@ -1,21 +1,24 @@
 <template>
   <div>
-    <Cover :cover="cover" :credits="credits" />
-    <div class="text-center my-8">
-      <h1 class="font-bold">{{ title }}</h1>
-      <p>Hugo « Nekika » Jahnke</p>
-      <Tags :tags="tags" />
+    <div class="relative">
+      <div class="text-center absolute inset-x-0 bottom-0 bg-darkgrey pb-2">
+        <h1 class="font-bold">{{ title }}</h1>
+        <p class="text-lightgrey">Hugo « Nekika » Jahnke</p>
+        <Tags :tags="tags" />
+      </div>
+      <img :src="coverSrc" alt="cover">
     </div>
+    <p class="text-center text-xs text-lightgrey mt-2">
+      Cover : {{ author }} | Source : <a :href="source.link">{{ source.name }}</a>
+    </p>
   </div>
 </template>
 
 <script>
-import Cover from './Cover'
 import Tags from './Tags'
 
 export default {
   components: {
-    Cover,
     Tags
   },
   props: {
@@ -35,6 +38,11 @@ export default {
       type: Object,
       default: null
     }
+  },
+  computed: {
+    coverSrc () { return require(`~/assets/covers/${this.cover}`) },
+    author () { return this.credits.author },
+    source () { return this.credits.source }
   }
 }
 </script>
