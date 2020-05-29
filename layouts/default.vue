3088335462abe6b8bed421f311d7181614ccab79
Nekika <hugo.jahnke@gmail.com>
2020-05-30 00:11:31 +0200

    Editing global padding

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="px-4 md:px-16 lg:px-24 xl:px-32 bg-grey text-white">
+  <div class="px-4 pb-10 md:px-24 lg:px-48 xl:px-64 bg-grey text-white">
     <Header />
-    <nuxt class="mt-4 md:mt-0" />
+    <nuxt class="mt-2" />
   </div>
 </template>
