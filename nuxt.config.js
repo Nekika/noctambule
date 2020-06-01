f352395cb42d15f90acd5c374e501f5a79dbbd90
Nekika <hugo.jahnke@gmail.com>
2020-06-01 18:19:41 +0200

    Preparing for deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,5 +68,12 @@ export default {
     */
     extend (config, ctx) {
     }
+  },
+  generate: {
+    async routes () {
+      const { $content } = require('@nuxt/content')
+      const files = await $content().only(['path']).fetch()
+      return files.map(file => file.path === '/index' ? '/' : file.path)
+    }
   }
 }
